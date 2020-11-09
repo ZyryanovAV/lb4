90230782c947ae706248fa2dbd7868ef6a3d196c
ZyryanovAV <ar200103zeranov@yandex.ru>
2020-11-09 16:40:52 +0300

    Add all file

--- individ3.py
+++ individ3.py
@@ -1,6 +1,9 @@
-i = 10
-while i <= 99:
-    s = int((i % 10) + (i / 10))
-    if i == s + s * s:
-        print("Это число:", i)
-    i += 1
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    i = 10
+    while i <= 99:
+        s = int((i % 10) + (i / 10))
+        if i == s + s * s:
+            print("Это число:", i)
+        i += 1
