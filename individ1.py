9bfd30c83639d9352bdeff9be242ce2d1beafe70
ZyryanovAV <ar200103zeranov@yandex.ru>
2020-11-10 20:33:11 +0300

    Add all file

--- individ1.py
+++ individ1.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import sys
+
 if __name__ == '__main__':
     N = int(input("Введите число экзаменов -> "))
-        if N <= 20:
-            if N == 1:
-                print(f"Мы успешно сдали {N} экзамен")
-            elif 2 <= N <= 4:
-                print(f"Мы успешно сдали {N} экзаменa")
-            elif 5 <= N <= 20:
-                print(f"Мы успешно сдали {N} экзаменов")
-        else:
-            print("Ошибка!")
-        exit(1)
+if N <= 20:
+    if N == 1:
+        print(f"Мы успешно сдали {N} экзамен")
+    elif 2 <= N <= 4:
+        print(f"Мы успешно сдали {N} экзаменa")
+    elif 5 <= N <= 20:
+        print(f"Мы успешно сдали {N} экзаменов")
+else:
+    print("Ошибка!")
+sys.exit(1)
